6a15930ac1360bf23c2f48a6b2f5e4a72c75e844
Kishore Annambhotla <k4annam@gmail.com>
2026-03-12 00:49:15 -0400

    finish preprocessing in data_ingestion, next is to fix data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 from src.exception import CustomException
 from src.logger import logging
@@ -34,8 +35,8 @@ class DataIngestion:
 
             # Before splitting: drop columns, rename winner/loser, scramble winner/loser
             df = self.drop_columns(df)
-            #rename_winner_loser()
-            #scramble_winner_loser()
+            df = self.rename_winner_loser(df)
+            df = self.scramble_winner_loser(df)
 
             # Initialize directories for the three CSVs
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
@@ -53,6 +54,7 @@ class DataIngestion:
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path,
             )
+        
         except Exception as e:
             raise CustomException(e, sys)
     
@@ -64,17 +66,40 @@ class DataIngestion:
                            'match_num', 'best_of', 'round', 'minutes', 'score', 'winner_entry', 'loser_entry', 
                            'winner_rank', 'loser_rank', 'w_bpSaved', 'l_bpSaved']
         df = df.drop(columns=columns_to_drop)
-        # also drop columns with nan values
-        df.dropna(axis=1, inplace=True)
+        # also drop rows with nan values
+        df.dropna()
         return df
 
     # helper function: rename winner/loser columns to player1/player2
     def rename_winner_loser(self, df):
-        pass
+        for col in df.columns:
+            if 'winner' in col:
+                df.rename(columns={col: col.replace('winner', 'p1')}, inplace=True)
+            elif 'w' in col:
+                df.rename(columns={col: col.replace('w', 'p1')}, inplace=True)
+            elif 'loser' in col:
+                df.rename(columns={col: col.replace('loser', 'p2')}, inplace=True)
+            elif 'l' in col:
+                df.rename(columns={col: col.replace('l', 'p2')}, inplace=True)
+        df['match_winner'] = 0 # create match winner column with all 0s (player 1 for all obs.)
+        return df
 
     # helper function: scramble winner/loser columns to remove bias
     def scramble_winner_loser(self, df):
-        pass
+        cols1 = [c for c in df.columns if 'p1' in c]
+        cols2 = [c for c in df.columns if 'p2' in c]
+        cols2_targ = [c.replace('p1', 'p2') for c in cols1]
+
+        # copy df for scrambling
+        copy = df.copy()
+
+        # generate len(df.index) / 2 random indices. for those observations, swap columns and set match_winner to 1
+        maskIdx = [random.randint(0, len(df.index) - 1) for i in range((int)(len(df.index) / 2))]
+        df.loc[maskIdx, cols1] = copy.loc[maskIdx, cols2_targ].values
+        df.loc[maskIdx, cols2_targ] = copy.loc[maskIdx, cols1].values
+        df.loc[maskIdx, 'match_winner'] = 1
+
+        return df
 
         
 if __name__ == "__main__":
